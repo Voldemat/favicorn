6e3eafa0b667b51ccf96eec89fe913e42a02d352
Владимир Войтенко <vladimirdev635@gmail.com>
2023-03-26 22:05:50 +0500

    refactoring

--- favicorn/asgi_controller.py
+++ favicorn/asgi_controller.py
@@ -1,3 +1,6 @@
+import enum
+from typing import Iterable, cast
+
 from asgiref.typing import (
     ASGI3Application,
     ASGIReceiveEvent,
@@ -7,6 +10,7 @@ from asgiref.typing import (
     HTTPRequestEvent,
     HTTPResponseBodyEvent,
     HTTPResponseStartEvent,
+    HTTPResponseTrailersEvent,
     HTTPScope,
 )
 
@@ -14,11 +18,17 @@ from .request_parser import HTTPRequestParser
 from .response_serializer import HTTPResponseSerializer
 
 
+class HTTPResponseEvents(enum.Enum):
+    START = "http.response.start"
+    TRAILERS = "http.response.trailers"
+    BODY = "http.response.body"
+
+
 class ASGIController:
     app: ASGI3Application
-    body: bytes | None
     request_parser: HTTPRequestParser
     response_serializer: HTTPResponseSerializer
+    expected_event: HTTPResponseEvents | None
 
     def __init__(
         self,
@@ -29,6 +39,7 @@ class ASGIController:
         self.app = app
         self.request_parser = request_parser
         self.response_serializer = response_serializer
+        self.expected_event = HTTPResponseEvents.START
 
     async def start(self) -> None:
         request = await self.request_parser.get_request()
@@ -63,9 +74,6 @@ class ASGIController:
             more_body=more_body,
         )
 
-    async def send(self, event: ASGISendEvent) -> None:
-        self.response_serializer.send(event)
-
     async def send_500_response(self) -> None:
         await self.send(
             HTTPResponseStartEvent(
@@ -82,3 +90,48 @@ class ASGIController:
                 more_body=False,
             )
         )
+
+    async def send(self, event: ASGISendEvent) -> None:
+        self.validate_event_type(event["type"])
+        match event["type"]:
+            case HTTPResponseEvents.START.value:
+                event = cast(HTTPResponseStartEvent, event)
+                trailers = event.get("trailers", False)
+                if trailers is True:
+                    self.expected_event = HTTPResponseEvents.TRAILERS
+                else:
+                    self.expected_event = HTTPResponseEvents.BODY
+                self.response_serializer.start(
+                    http_version="1.1",
+                    status=event["status"],
+                    headers=event["headers"],
+                    more_headers=trailers,
+                )
+            case HTTPResponseEvents.TRAILERS.value:
+                event = cast(HTTPResponseTrailersEvent, event)
+                more_trailers = event.get("more_trailers", False)
+                if more_trailers is False:
+                    self.expected_event = HTTPResponseEvents.BODY
+                self.response_serializer.send_extra_headers(
+                    event["headers"],
+                    more_trailers,
+                )
+            case HTTPResponseEvents.BODY.value:
+                event = cast(HTTPResponseBodyEvent, event)
+                more_body = event.get("more_body", False)
+                if more_body is False:
+                    self.expected_event = None
+                self.response_serializer.send_body(
+                    body=event["body"],
+                    more_body=more_body,
+                )
+            case _:
+                self.response_serializer.close()
+                raise RuntimeError(f"Unhandled event type: {event['type']}")
+
+    def encode_headers(self, headers: Iterable[tuple[bytes, bytes]]) -> bytes:
+        return b"".join(map(lambda h: h[0] + b": " + h[1] + b"\n", headers))
+
+    def validate_event_type(self, event_type: str) -> None:
+        assert self.expected_event is not None, "Response already ended"
+        assert event_type == self.expected_event.value
--- favicorn/response_serializer.py
+++ favicorn/response_serializer.py
@@ -1,68 +1,49 @@
 import asyncio
-import enum
-from typing import Iterable, cast
-
-from asgiref.typing import (
-    ASGISendEvent,
-    HTTPResponseBodyEvent,
-    HTTPResponseStartEvent,
-    HTTPResponseTrailersEvent,
-)
-
-
-class HTTPResponseEvents(enum.Enum):
-    START = "http.response.start"
-    TRAILERS = "http.response.trailers"
-    BODY = "http.response.body"
+from typing import Iterable
 
 
 class HTTPResponseSerializer:
-    expected_event: HTTPResponseEvents | None
     transport: asyncio.WriteTransport
 
     def __init__(self, transport: asyncio.WriteTransport) -> None:
-        self.expected_event = HTTPResponseEvents.START
         self.transport = transport
 
-    def is_completed(self) -> bool:
-        return self.expected_event is None
+    def start(
+        self,
+        http_version: str,
+        status: int,
+        headers: Iterable[tuple[bytes, bytes]],
+        more_headers: bool,
+    ) -> None:
+        message = (
+            f"HTTP/{http_version} {status}\n".encode()
+            + self.encode_headers(headers)
+        )
+        if more_headers is False:
+            message += b"\n"
+        self.transport.write(message)
 
-    def send(self, event: ASGISendEvent) -> None:
-        self.validate_event(event)
-        match event["type"]:
-            case HTTPResponseEvents.START.value:
-                event = cast(HTTPResponseStartEvent, event)
-                headers = self.encode_headers(event["headers"])
-                message: bytes = (
-                    f'HTTP/1.1 {event["status"]}\n'.encode() + headers
-                )
-                if event.get("trailers", False) is True:
-                    self.expected_event = HTTPResponseEvents.TRAILERS
-                else:
-                    self.expected_event = HTTPResponseEvents.BODY
-                    message += b"\n"
-                self.transport.write(message)
-            case HTTPResponseEvents.TRAILERS.value:
-                event = cast(HTTPResponseTrailersEvent, event)
-                message = self.encode_headers(event["headers"])
-                if event.get("more_trailers", False) is False:
-                    message += b"\n"
-                    self.expected_event = HTTPResponseEvents.BODY
-                self.transport.write(message)
-            case HTTPResponseEvents.BODY.value:
-                event = cast(HTTPResponseBodyEvent, event)
-                message = event["body"]
-                self.transport.write(message)
-                if event.get("more_body", False) is False:
-                    self.expected_event = None
-                    self.transport.write_eof()
-            case _:
-                self.transport.write_eof()
-                raise RuntimeError(f"Unhandled event type: {event['type']}")
+    def send_extra_headers(
+        self,
+        headers: Iterable[tuple[bytes, bytes]],
+        more_headers: bool,
+    ) -> None:
+        message = self.encode_headers(headers)
+        if more_headers is False:
+            message += b"\n"
+        self.transport.write(message)
+
+    def send_body(
+        self,
+        body: bytes,
+        more_body: bool,
+    ) -> None:
+        self.transport.write(body)
+        if more_body is False:
+            self.transport.write_eof()
+
+    def close(self) -> None:
+        self.transport.write_eof()
 
     def encode_headers(self, headers: Iterable[tuple[bytes, bytes]]) -> bytes:
         return b"".join(map(lambda h: h[0] + b": " + h[1] + b"\n", headers))
-
-    def validate_event(self, event: ASGISendEvent) -> None:
-        assert self.expected_event is not None, "Response already ended"
-        assert event["type"] == self.expected_event.value
